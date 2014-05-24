1d5094c99d653613ec54730ef1edb9d919b81365
Larry Fox <larrywfox@gmail.com>
2014-05-24 15:51:05 -0400

    silence julia

--- julia/.zsh/julia.zsh
+++ julia/.zsh/julia.zsh
@@ -1,1 +1,1 @@
-alias julia="/opt/homebrew-cask/Caskroom/julia/0.2.1/Julia-0.2.1.app/Contents/Resources/julia/bin/julia"
+alias julia="/opt/homebrew-cask/Caskroom/julia/0.2.1/Julia-0.2.1.app/Contents/Resources/julia/bin/julia -q "

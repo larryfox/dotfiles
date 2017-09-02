3ef472c2c220040fa6bec13b303d39651f551c3e
Larry Fox <l@rryfox.us>
2017-09-02 15:04:55 -0400

    Remove julia configs

--- julia/.juliarc.jl
+++
@@ -1,9 +0,0 @@
-if isinteractive()
-    function customize_promp(repl)
-        repl.interface = Base.REPL.setup_interface(repl)
-        repl.interface.modes[1].first_prompt = \
-            repl.interface.modes[1].prompt = " ∴ "
-    end
-
-    atreplinit(customize_promp)
-end
--- julia/.zsh/julia.zsh
+++
@@ -1,1 +0,0 @@
-alias julia="/Users/fox/Applications/Julia-0.4.5.app/Contents/Resources/julia/bin/julia -q "

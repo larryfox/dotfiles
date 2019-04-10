da83b082009373a5d0500c43e33b214357641461
Larry Fox <l@rryfox.us>
2019-04-10 17:14:59 -0400

    Don't source chruby if it doesn't exist

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,6 @@ set PROJECTS $HOME/Code
 
 set CDPATH . $PROJECTS $CDPATH
 
-source /usr/local/share/chruby/chruby.fish
+if test -f /usr/local/share/chruby/chruby.fish
+    source /usr/local/share/chruby/chruby.fish
+end

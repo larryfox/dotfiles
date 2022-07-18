e36fa5c6658e9aef36a2baf1150a951a6f917571
Larry Fox <git@larryfox.org>
2022-07-18 17:06:02 -0600

    fish prompt

--- fish/.config/fish/conf.d/promt.fish
+++ fish/.config/fish/conf.d/promt.fish
@@ -5,22 +5,21 @@ end
 # Remove greeting
 set fish_greeting
 
+# Turn on git prompt features
+set __fish_git_prompt_char_stateseparator " "
+set __fish_git_prompt_showdirtystate
+set __fish_git_prompt_char_dirtystate "✗"
+set __fish_git_prompt_color_dirtystate --bold red
+set __fish_git_prompt_char_stagedstate "±"
+set __fish_git_prompt_color_stagedstate --bold green
+set __fish_git_prompt_color 666
+
 function fish_prompt
     printf ' ❯ '
 end
 
 function fish_right_prompt
     printf '%s%s%s%s%s%s' (set_color blue) (prompt_pwd) \
-      (set_color 666) (__fish_git_prompt) (___git_dirty) \
+      (fish_git_prompt) \
       (set_color normal)
-end
-
-function ___git_dirty
-    set -l dirty
-
-    command git diff-index --quiet --ignore-submodules HEAD ^/dev/null
-
-    if test $status -eq 1
-        printf '%s%s%s' (set_color red) " ✗" (set_color normal)
-    end
 end

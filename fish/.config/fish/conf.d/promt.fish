295b9cffa0d4023121369d4dcde4bc9a28aa10bb
Larry Fox <git@larryfox.org>
2024-05-04 21:41:02 -0600

    update prompt

--- fish/.config/fish/conf.d/promt.fish
+++ fish/.config/fish/conf.d/promt.fish
@@ -12,14 +12,19 @@ set __fish_git_prompt_char_dirtystate "✗"
 set __fish_git_prompt_color_dirtystate --bold red
 set __fish_git_prompt_char_stagedstate "±"
 set __fish_git_prompt_color_stagedstate --bold green
-set __fish_git_prompt_color 666
+set __fish_git_prompt_color_branch --bold green
+# set __fish_git_prompt_color --bold 666
 
 function fish_prompt
-    printf ' ❯ '
+  printf '%s%s%s ❯ ' \
+    (set_color $fish_color_cwd) \
+    (path basename (prompt_pwd)) \
+    (set_color normal)
 end
 
 function fish_right_prompt
-    printf '%s%s%s%s%s%s' (set_color blue) (prompt_pwd) \
-      (fish_git_prompt) \
-      (set_color normal)
+  printf '%s %s%s%s' (fish_git_prompt) \
+    (set_color brgrey) \
+    (date "+%R") \
+    (set_color normal)
 end

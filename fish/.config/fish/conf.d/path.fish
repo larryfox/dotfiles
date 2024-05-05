97745c38dc9ad96556d1da957ff9e90ddc59a939
Larry Fox <git@larryfox.org>
2024-05-04 21:40:07 -0600

    update $PATH

--- fish/.config/fish/conf.d/path.fish
+++ fish/.config/fish/conf.d/path.fish
@@ -2,24 +2,16 @@
 set -gx HOMEBREW_PREFIX "/opt/homebrew"
 set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar"
 set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
-set -gx MANPATH "/opt/homebrew/share/man" $MANPATH
-set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH
+set -gxp MANPATH "/opt/homebrew/share/man"
+set -gxp INFOPATH "/opt/homebrew/share/info"
 
-contains /opt/homebrew/bin $fish_user_paths; or set -Up fish_user_paths /opt/homebrew/bin
-contains /opt/homebrew/sbin $fish_user_paths; or set -Up fish_user_paths /opt/homebrew/sbin
+fish_add_path $HOME/.local/bin
+fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
 
 # GNU core utilities without 'g' prefix (brew --prefix coreutils)
-contains /opt/homebrew/opt/coreutils/libexec/gnubin $fish_user_paths; or set -Up fish_user_paths /opt/homebrew/opt/coreutils/libexec/gnubin
+fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin
 set -gxp MANPATH /opt/homebrew/opt/coreutils/libexec/gnuman
 
-# sqlite
-# set -U fish_user_paths /usr/local/opt/sqlite/bin $fish_user_paths
-
-# mysql
-# set -U fish_user_paths /usr/local/opt/mysql-client/bin $fish_user_paths
-
-# Go
-# set fish_user_paths $GOPATH/bin $fish_user_paths
-
-# Local bins
-contains $HOME/bin $fish_user_paths; or set -Up fish_user_paths $HOME/bin
+fish_add_path $GOPATH/bin
+fish_add_path $HOME/.cargo/bin

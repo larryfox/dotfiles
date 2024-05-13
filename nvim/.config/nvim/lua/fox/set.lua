73b4b9cf08326342990f87ba0580c609f77d3ed3
Larry Fox <git@larryfox.org>
2024-05-12 21:04:28 -0600

    indenting

--- nvim/.config/nvim/lua/fox/set.lua
+++ nvim/.config/nvim/lua/fox/set.lua
@@ -25,10 +25,14 @@ vim.opt.scrolloff = 5
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-vim.opt.timeoutlen = 500
 
 vim.opt.colorcolumn = "80"
+
 vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
